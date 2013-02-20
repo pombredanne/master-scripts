5c7a675a41d9aa298b94204f79c508f37d1b7e7f
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-02-20 16:34:10 +0100

    Add setup files

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+#
+# Copyright 2009-2012 Ghent University
+#
+# This file is part of VSC-tools,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/VSC-tools
+#
+# VSC-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# VSC-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VSC-tools. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Basic setup.py for building the hpcugent Icinga checks"""
+
+import sys
+import os
+from distutils.core import setup
+import glob
+
+setup(name="muk_scripts",
+      version="1.0",
+      description="UGent HPC scripts that should be deployed on the muk",
+      long_description="""Scripts that run on the muk masters.
+- GPFS quota checking and caching
+- Queue information caching for the users
+- PBS queue monitoring for inactive users
+
+Overlaps with master_scripts, and should be merged back into it ASAP.
+""",
+      license="LGPL",
+      author="HPC UGent",
+      author_email="hpc-admin@lists.ugent.be",
+      scripts=glob.glob(os.path.join("files", "*")),
+      url="http://www.ugent.be/hpc")
