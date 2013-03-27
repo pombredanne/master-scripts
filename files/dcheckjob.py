3740b22ba8db443b331ec72f34cb15fc63b594ca
Andy Georges <itkovian@gmail.com>
2013-03-27 17:05:05 +0100

    Euh yeah, arguments should be correctly named when moving around

--- files/dcheckjob.py
+++ files/dcheckjob.py
@@ -99,7 +99,7 @@ def main():
             'path': checkjob_path
         }
 
-    checkjob = Checkjob(clusters, cache_pickle=True, opts.options.dry_run=True)
+    checkjob = Checkjob(clusters, cache_pickle=True, dry_run=True)
 
     (job_information, reported_hosts, failed_hosts) = checkjob(opts)
     timeinfo = time.time()
