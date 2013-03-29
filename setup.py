d5a3f4c0cb39de4ff0942d36a8b7e50a04ade49d
Andy Georges <itkovian@gmail.com>
2013-03-29 15:06:48 +0100

    Upgraded the setup.py script to the common format

--- setup.py
+++ setup.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
+##
+# Copyright 2009-2013 Ghent University
 #
-# Copyright 2009-2012 Ghent University
-#
-# This file is part of VSC-tools,
+# This file is part of vsc-config,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
 # the Hercules foundation (http://www.herculesstichting.be/in_English)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# http://github.com/hpcugent/VSC-tools
+# All rights reserved.
 #
-# VSC-tools is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
-#
-# VSC-tools is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with VSC-tools. If not, see <http://www.gnu.org/licenses/>.
-#
-"""Basic setup.py for building the hpcugent Icinga checks"""
+##
+"""Basic setup.py for master scripts"""
 
-import sys
-import os
 from distutils.core import setup
-import glob
 
-setup(name="master_scripts",
-      version="1.1.1",
-      description="UGent HPC scripts that should be deployed on the masters",
-      long_description="""Scripts that run on one or more masters
- - GPFS quota checking and caching
- - Queue information caching for the users
- - PBS queue monitoring for inactive users
-""",
-      license="LGPL",
-      author="HPC UGent",
-      author_email="hpc-admin@lists.ugent.be",
-      scripts=glob.glob(os.path.join("files", "*")),
-      url="http://www.ugent.be/hpc")
+import vsc.install.shared_setup
+from vsc.install.shared_setup import ag, sdw, wdp, kh
+
+
+PACKAGE = {
+    'name': 'master_scripts',
+    'version': '1.3',
+    'author': [ag, kh, sdw, wdp],
+    'description': 'UGent HPC scripts that should be deployed on the masters',
+    'license': 'LGPL',
+    'packages': ['vsc', 'vsc.utils'],
+    'scripts': ['bin/pbs_check_inactive_user-jobs.py', 'bin/dshowq.py', 'bin/quota_check_user_notification.py'],
+}
+
+
+if __name__ == '__main__':
+    shared_setup.action_target(PACKAGE)
